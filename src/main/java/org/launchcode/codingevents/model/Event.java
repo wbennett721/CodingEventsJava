09bb0a6c3414938694ea92e849e0aa4c599bdf4d
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2024-01-25 16:36:15 -0600

    codingevent

--- src/main/java/org/launchcode/codingevents/model/Event.java
+++ src/main/java/org/launchcode/codingevents/model/Event.java
@@ -1,11 +1,32 @@
 package org.launchcode.codingevents.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
 public class Event {
 
+    private int id;
+    private static int nextId = 1;
+
+    @NotBlank
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
 
-    public Event(String name) {
+    @Size(max = 500, message = "Description too long!")
+    private String description;
+
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
+        this.description = description;
+        this.contactEmail = contactEmail;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -16,8 +37,41 @@ public class Event {
         this.name = name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
